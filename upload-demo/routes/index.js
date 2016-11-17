24acfb6606f71fb8726127cd4dca882b81decb33
CleverFan <377109151@qq.com>
2016-11-17 21:58:35 +0800

    Initial commit

---
+++ upload-demo/routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/home',function (req,res,next) {
+  res.writeHead(302,{
+    'Location':'http://www.improvecfan.cn'
+  });
+  res.end();
+});
+
+router.get('/goany',function (req,res,next) {
+  var url = [
+    'http://www.baidu.com',
+    'http://www.improvecfan.cn',
+    'http://blog.improvecfan.cn',
+    'https://github.com',
+    'https://github.com/CleverFan',
+    'http://blog.csdn.net/qq_31655965',
+    'https://www.google.com.hk/?gws_rd=ssl',
+    'http://www.htmleaf.com/',
+    'https://www.zhihu.com',
+    'http://www.runoob.com/nodejs/nodejs-tutorial.html'
+  ]
+  var randomNum = parseInt(10*Math.random());
+  res.writeHead(302,{
+    'Location': url[randomNum]
+  });
+  res.end();
+});
+
+
+module.exports = router;
